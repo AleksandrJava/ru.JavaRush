697fecfa5e7696e0a933dc02734f83b73a98fe77
AleksandrJava <44460921+AleksandrJava@users.noreply.github.com>
2019-08-20 17:31:33 +0400

    end 21 lesson

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,69 @@
+package com.javarush.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private List<Horse> horses;
+    static Hippodrome game;
+    private Horse winner;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Horse horse1 = new Horse("horse1", 3, 0);
+        Horse horse2 = new Horse("horse2", 3, 0);
+        Horse horse3 = new Horse("horse3", 3, 0);
+        List<Horse> list = new ArrayList<>();
+        list.add(horse1);
+        list.add(horse2);
+        list.add(horse3);
+        game = new Hippodrome(list);
+        game.run();
+        game.printWinner();
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 1; i <= 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+    }
+
+    public void move(){
+        for (Horse horse: horses) {
+            horse.move();
+        }
+    }
+
+    public void print(){
+        for (Horse horse: horses) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public Horse getWinner(){
+        double max = 0;
+        for (Horse horse: horses) {
+            if(horse.distance > max){
+                max = horse.distance;
+                winner = horse;
+            }
+        }
+        return winner;
+    }
+
+    public void printWinner(){
+        System.out.println("Winner is " + getWinner().getName() + "!");
+    }
+}
